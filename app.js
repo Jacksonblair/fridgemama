ea2e52909e14f1096c6f4c97036fe4c175907c4b
Jacksonblair <jackson.blair@live.com>
2019-09-30 21:07:39 +1000

    update frontend

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Cache-control
+	// TO-DO
+
 // sessions
 app.use(sessions({
 	cookieName: "session",
